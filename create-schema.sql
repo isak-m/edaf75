daeb7e6a15cf6b4cb9ccb20265b29ceeaa083180
Edward Sjöblom <edward.sjoblom@gmail.com>
2020-04-22 08:13:51 +0000

    Update create-schema.sql

---
+++ create-schema.sql
@@ -0,0 +1,40 @@
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS pallets;
+DROP TABLE IF EXISTS deliveries;
+DROP TABLE IF EXISTS order_specs;
+DROP TABLE IF EXISTS recipe_items;
+DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS cookies;
+DROP TABLE IF EXISTS ingredients;
+
+CREATE TABLE orders(
+
+);
+
+CREATE TABLE pallets(
+
+);
+
+CREATE TABLE deliveries(
+
+);
+
+CREATE TABLE order_specs(
+
+);
+
+CREATE TABLE recipe_items(
+
+);
+
+CREATE TABLE customers(
+
+);
+
+CREATE TABLE cookies(
+
+);
+
+CREATE TABLE ingredients(
+
+);
